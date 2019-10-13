c0c958fabc256b39733c0341fde236b0bb7b784f
ctetsass <conrard.tetsassi@unamur.be>
2019-10-13 13:35:28 -0400

    Create NHANES.R and NAHNES.Rmd

---
+++ NHANES.R
@@ -0,0 +1,120 @@
+#setwd("~/Documents/ML-UYork/ML1000/Asignement2")
+library(plyr)
+library(dplyr)
+library(tidyr)
+#library(tidyverse)
+library(knitr)    # For knitting document and include_graphics function
+library(ggplot2)  # For plotting
+# Reading files
+
+#mydata <- read.csv(file.choose(), header = TRUE, na.strings = c("NA","","#NA"))
+# Reading files
+demographic   = read.csv("Data/Raw/demographic.csv", header = TRUE, na.strings = c("NA","","#NA"))
+diet          = read.csv("Data/Raw/diet.csv", header = TRUE, na.strings = c("NA","","#NA"))
+examination   = read.csv("Data/Raw/examination.csv", header = TRUE, na.strings = c("NA","","#NA"))
+labs          = read.csv("Data/Raw/labs.csv", header = TRUE, na.strings = c("NA","","#NA"))
+medications   = read.csv("Data/Raw/medications.csv", header = TRUE, na.strings = c("NA","","#NA"))
+questionnaire = read.csv("Data/Raw/questionnaire.csv", header = TRUE, na.strings = c("NA","","#NA"))
+
+
+# Merging files
+data_List = list(demographic,examination,diet,labs,questionnaire,medications)
+Data_joined = join_all(data_List) #require(plyr)
+
+
+
+# Data Exploration
+#Check the data for missing values.
+attach(demographic)
+str(demographic)
+sapply(demographic, function(x) sum(is.na(x)))
+
+
+################## demographic_MS : MS stand for missing data ####################
+
+demographic_MS <- demographic %>% summarise_all(~(sum(is.na(.))/n()))
+demographic_MS <- gather(demographic_MS, key = "variables", value = "percent_missing")
+demographic_MS <- demographic_MS[demographic_MS$percent_missing > 0.0, ] 
+ggplot(demographic_MS, aes(x = reorder(variables, percent_missing), y = percent_missing)) +
+  geom_bar(stat = "identity", fill = "blue", aes(color = I('white')), size = 0.3, alpha = 0.8)+
+  xlab('variables')+
+  coord_flip()+ 
+  #theme_fivethirtyeight() +
+  ggtitle("Demographic Missing Data By Columns")
+
+
+##################   diet_MS : MS stand for missing data      ####################
+diet_MS <- diet %>% summarise_all(~(sum(is.na(.))/n()))
+diet_MS <- gather(diet_MS, key = "variables", value = "percent_missing")
+diet_MS <- diet_MS[diet_MS$percent_missing > 0.0, ] 
+ggplot(diet_MS, aes(x = reorder(variables, percent_missing), y = percent_missing)) +
+  geom_bar(stat = "identity", fill = "blue", aes(color = I('white')), size = 0.3, alpha = 0.8)+
+  xlab('variables')+
+  coord_flip()+ 
+  #theme_fivethirtyeight() +
+  ggtitle("Diet Missing Data By Columns")
+################## examination_MS : MS stand for missing data ####################
+
+examination_MS <- examination %>% summarise_all(~(sum(is.na(.))/n()))
+examination_MS <- gather(examination_MS, key = "variables", value = "percent_missing")
+examination_MS <- examination_MS[examination_MS$percent_missing > 0.0, ] 
+ggplot(examination_MS, aes(x = reorder(variables, percent_missing), y = percent_missing)) +
+  geom_bar(stat = "identity", fill = "blue", aes(color = I('white')), size = 0.3, alpha = 0.8)+
+  xlab('variables')+
+  coord_flip()+ 
+  #theme_fivethirtyeight() +
+  ggtitle("Examination Missing Data By Columns")
+
+##################    labs_MS : MS stand for missing data     ####################
+
+sapply(labs, function(x) sum(is.na(x)))
+
+labs_MS <- labs %>% summarise_all(~(sum(is.na(.))/n()))
+labs_MS <- gather(labs_MS, key = "variables", value = "percent_missing")
+labs_MS <- labs_MS[labs_MS$percent_missing > 0.0, ] 
+ggplot(labs_MS, aes(x = reorder(variables, percent_missing), y = percent_missing)) +
+  geom_bar(stat = "identity", fill = "blue", aes(color = I('white')), size = 0.3, alpha = 0.8)+
+  xlab('variables')+
+  coord_flip()+ 
+  #theme_fivethirtyeight() +
+  ggtitle(" Labs Missing Data By Columns")
+
+################## medications_MS : MS stand for missing data ####################
+
+medications_MS <- medications %>% summarise_all(~(sum(is.na(.))/n()))
+medications_MS <- gather(medications_MS, key = "variables", value = "percent_missing")
+medications_MS <- medications_MS[medications_MS$percent_missing > 0.0, ] 
+ggplot(medications_MS, aes(x = reorder(variables, percent_missing), y = percent_missing)) +
+  geom_bar(stat = "identity", fill = "blue", aes(color = I('white')), size = 0.3, alpha = 0.8)+
+  xlab('variables')+
+  coord_flip()+ 
+  #theme_fivethirtyeight() +
+  ggtitle("Medications Missing Data By Columns")
+
+################## questionnaire_MS : MS stand for missing data ##################
+
+questionnaire_MS <- questionnaire %>% summarise_all(~(sum(is.na(.))/n()))
+questionnaire_MS <- gather(questionnaire_MS, key = "variables", value = "percent_missing")
+questionnaire_MS <- questionnaire_MS[questionnaire_MS$percent_missing > 0.0, ] 
+ggplot(questionnaire_MS, aes(x = reorder(variables, percent_missing), y = percent_missing)) +
+  geom_bar(stat = "identity", fill = "blue", aes(color = I('white')), size = 0.3, alpha = 0.8)+
+  xlab('variables')+
+  coord_flip()+ 
+  #theme_fivethirtyeight() +
+  ggtitle("Questionnaire Missing Data By Columns")
+ 
+
+
+
+
+
+
+
+
+
+
+#Define the predictors
+#Train the model
+#Evaluate the model
+
+
