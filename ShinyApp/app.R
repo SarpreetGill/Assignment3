9cb811b3c6e8b09da2885c7198151cf5615f8c2b
Andre Schardong <andreschardong@usp.br>
2019-11-14 21:23:32 -0500

    Shiny

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -25,46 +25,50 @@ m <- list(
 
 #setwd('D:/ProjetosGIT/Assignment3/ShinyApp')
 #prepare_data_frame <- function(data_source) {
-individuals_transaction_class <- read.transactions('../Data/Working/transactiondata.csv', format = 'basket',sep=',')
+#individuals_transaction_class <- read.transactions('../Data/Working/transactiondata.csv', format = 'basket',sep=',')
 
-has_cancer.association.rules <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.4, maxlen=15), appearance=list(default="lhs", rhs="HAS CANCER"))
-has_diabetes.association.rules <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.7, maxlen=15), appearance=list(default="lhs", rhs="HAS DIABETES"))
-has_hypertension.association.rules <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.8, maxlen=15), appearance=list(default="lhs", rhs="HAS HYPERTENSION"))
+#has_cancer.association.rules <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.4, maxlen=15), appearance=list(default="lhs", rhs="HAS CANCER"))
+#has_diabetes.association.rules <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.7, maxlen=15), appearance=list(default="lhs", rhs="HAS DIABETES"))
+#has_hypertension.association.rules <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.8, maxlen=15), appearance=list(default="lhs", rhs="HAS HYPERTENSION"))
 
-has_cancer.association.rules_smallitemset <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.1, maxlen=3), appearance=list(default="lhs", rhs="HAS CANCER"))
-has_diabetes.association.rules_smallitemset <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.4, maxlen=3), appearance=list(default="lhs", rhs="HAS DIABETES"))
-has_hypertension.association.rules_smallitemset <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.4, maxlen=3), appearance=list(default="lhs", rhs="HAS HYPERTENSION"))
+#has_cancer.association.rules_smallitemset <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.1, maxlen=3), appearance=list(default="lhs", rhs="HAS CANCER"))
+#has_diabetes.association.rules_smallitemset <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.4, maxlen=3), appearance=list(default="lhs", rhs="HAS DIABETES"))
+#has_hypertension.association.rules_smallitemset <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.4, maxlen=3), appearance=list(default="lhs", rhs="HAS HYPERTENSION"))
 
-no_cancer.association.rules <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.9,maxlen=10), appearance=list(default="lhs", rhs="NO CANCER"))
-no_diabetes.association.rules <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.9, maxlen=10), appearance=list(default="lhs", rhs="NO DIABETES"))
-no_hypertension.association.rules <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.8, maxlen=10), appearance=list(default="lhs", rhs="NO HYPERTENSION"))
+#no_cancer.association.rules <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.9,maxlen=10), appearance=list(default="lhs", rhs="NO CANCER"))
+#no_diabetes.association.rules <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.9, maxlen=10), appearance=list(default="lhs", rhs="NO DIABETES"))
+#no_hypertension.association.rules <- apriori(individuals_transaction_class, parameter = list(supp=0.001, conf=0.8, maxlen=10), appearance=list(default="lhs", rhs="NO HYPERTENSION"))
 
+#top20cancerrules <- head(has_cancer.association.rules, n=20, by="confidence")
+#top20cancerrules_smallitemset <- head(has_cancer.association.rules_smallitemset, n=20, by="confidence")
 
-top20cancerrules <- head(has_cancer.association.rules, n=20, by="confidence")
-top20cancerrules_smallitemset <- head(has_cancer.association.rules_smallitemset, n=20, by="confidence")
+#top20diabetesrules <- head(has_diabetes.association.rules, n=20, by="confidence")
+#top20diabetesrules_smallitemset <- head(has_diabetes.association.rules_smallitemset, n=20, by="confidence")
 
-top20diabetesrules <- head(has_diabetes.association.rules, n=20, by="confidence")
-top20diabetesrules_smallitemset <- head(has_diabetes.association.rules_smallitemset, n=20, by="confidence")
-
-top20hypertensionrules <- head(has_hypertension.association.rules, n=20, by="confidence")
-top20hypertensionrules_smallitemset <- head(has_hypertension.association.rules_smallitemset, n=20, by="confidence")
+#top20hypertensionrules <- head(has_hypertension.association.rules, n=20, by="confidence")
+#top20hypertensionrules_smallitemset <- head(has_hypertension.association.rules_smallitemset, n=20, by="confidence")
 
 
 
 #model_rf
-model_rf <- function() {
+model_corr_rf <- function() {
   
-  # Kmeans
-  model_rf = readRDS("model_rf.rds")
+  # LR
+  model_corr_rf = readRDS("model_corr_rf.rds")
   
-  return(model_rf)
+  return(model_corr_rf)
 }
 
 predict.model_rf <- function(data) {
 
-  pred <- predict(model_rf, data)
+  pred <- predict(model_corr_rf, data)
   
-  return(pred)
+  result <- "NO" # Default result
+  if (pred == "X1" ) {
+    result <- "YES"
+  }
+  return(result)
+  
 }
 
 # Define UI for application that draws a histogram
@@ -91,8 +95,8 @@ ui <- navbarPage(
           
           # Input: Decimal interval with step value ----
           sliderInput("LBXSGL", "Glucose, refrigerated serum (mg/dL)",
-                      min = 0, max = 1,
-                      value = 0.5, step = 0.1),
+                      min = 10, max = 1000,
+                      value = 500, step = 10),
           
           # Input: Specification of range within an interval ----
           selectizeInput('RXDUSE', 'In the past 30 days, did you take medication?',
@@ -100,17 +104,16 @@ ui <- navbarPage(
           
           # Input: Custom currency format for with basic animation ----
           sliderInput("LBDHDDSI", "HDL-Cholesterol (mmol/L)",
-                      min = 0, max = 100,
-                      value = 0, step = 25,
+                      min = 0, max = 5,
+                      value = 1.5, step = 0.1,
                       animate = TRUE),
           
           # Input: Animation with custom interval (in ms) ----
           # to control speed, plus looping
           sliderInput("RIDAGEYR", "Age in years:",
-                      min = 1, max = 120,
-                      value = 1, step = 5,
-                      animate =
-                        animationOptions(interval = 300, loop = TRUE))
+                      min = 15, max = 99,
+                      value = 50, step = 1,
+                      animate = TRUE)
         ),
         
         
@@ -119,11 +122,7 @@ ui <- navbarPage(
         mainPanel(
           
           h2("This patient is likely to:"),
-          h1("BE SICK"),
-          # Output: Table summarizing the values entered ----
-          #tableOutput("values")
-          htmlOutput("resHtml"),
-          tableOutput('values')
+          textOutput("resHtml")
           
         )
         
@@ -132,7 +131,7 @@ ui <- navbarPage(
   tabPanel("Interactive plots",
            fluidRow(column(
              12,
-             h2("Interactive Graph Visualization"),
+             h2("Data Explorer - Interactive Graph Visualization"),
              
              
              
@@ -142,9 +141,9 @@ ui <- navbarPage(
              h2("Cancer"),
              
              
-             plotly_arules(has_cancer.association.rules)
-             %>%
-               layout(autosize = F, width = "100%", height = 500, margin=m)
+             #plotly_arules(has_cancer.association.rules)
+             #%>%
+            #   layout(autosize = F, width = "100%", height = 500, margin=m)
              
            ))
            ,
@@ -152,9 +151,9 @@ ui <- navbarPage(
              12,
              h2("Diabetes"),
              
-             plotly_arules(has_diabetes.association.rules)
-             %>%
-               layout(autosize = F, width = "100%", height = 500, margin=m)
+             #plotly_arules(has_diabetes.association.rules)
+             #%>%
+            #   layout(autosize = F, width = "100%", height = 500, margin=m)
              
              
            ))
@@ -163,9 +162,9 @@ ui <- navbarPage(
              12,
              h2("Hypertension"),
              
-             plotly_arules(has_hypertension.association.rules)
-             %>%
-               layout(autosize = F, width = "100%", height = 500, margin=m)
+             #plotly_arules(has_hypertension.association.rules)
+             #%>%
+            #   layout(autosize = F, width = "100%", height = 500, margin=m)
              
            ))
            
@@ -182,33 +181,28 @@ ui <- navbarPage(
 # Define server logic required to draw a histogram
 server <- function(input, output,session) {
   
+  output$resHtml <- "Test"
   
   # Reactive expression to create data frame of all input values ----
   
   sliderValues <- reactive({
     
-    data.frame(
-      Name = c("LBXGH",
-               "LBXSGL",
-               "RXDUSE",
-               "LBDHDDSI",
-               "RIDAGEYR"),
-     Value = c(as.character(input$LBXGH),
-               as.character(input$LBXSGL),
-               input$RXDUSE,
-               as.character(input$LBDHDDSI),
-               as.character(input$RIDAGEYR))
-    )
+    data = data.frame(
+      LBXGH =as.numeric(input$LBXGH),
+      LBXSGL= as.numeric(input$LBXSGL),
+      RXDUSE= as.numeric( ifelse(input$RXDUSE=='YES', 2, 1)),
+      LBDHDDSI= as.numeric(input$LBDHDDSI),
+      RIDAGEYR = as.numeric(input$RIDAGEYR))
     
     #predic the model
-    predict.model_rf(data)    
+    fncPredictModel(data)
     
   })
   
   # Show the values in an HTML table ----
-  output$values <- renderTable({
-    sliderValues()
-  })
+  #output$values <- renderText({
+  #  sliderValues()
+  #})
   
   #kmeans.select_data.rds = readRDS("kmeans.select_data.rds")
   #kmeans.select_data.rds <- kmeans.select_data.rds %>%
@@ -216,12 +210,16 @@ server <- function(input, output,session) {
   
   fncPredictModel <- function(data) {
     
-    pred <- predict.model_rf(data)
+    pred_probs <- predict.model_rf(data)
+    
+    result <- "Has no Diabetes" # Default result
+    if (pred =="Yes" ) {
+      result <- "Has Diabetes"
+    }
     
     resHtml <- paste(
       sep = ,
-      "<h4>The coordinates you selected:</h4>",
-      "<h3>",pred,"</h3>",
+      "<h3>",result,"</h3>",
       "</b>"
     )
     
@@ -229,28 +227,20 @@ server <- function(input, output,session) {
       resHtml
     })
     
-    result <- resHtml
   }
   
-  observe({
-    data = c(as.character(input$LBXGH),
-              as.character(input$LBXSGL),
-              input$RXDUSE,
-              as.character(input$LBDHDDSI),
-              as.character(input$RIDAGEYR))
-  
-    
-    #predic the model
-      
-    fncPredictModel(data)
-  }) 
-  
-  
-  ## Data Explorer ###########################################
-  observe({
-
-      })
-  
+  #observe({
+  #  
+  #  data = data.frame(
+  #    LBXGH =as.numeric(input$LBXGH),
+  #    LBXSGL= as.numeric(input$LBXSGL),
+  #    RXDUSE= as.numeric( ifelse(input$RXDUSE=='YES', 2, 1)),
+  #    LBDHDDSI= as.numeric(input$LBDHDDSI),
+  #    RIDAGEYR = as.numeric(input$RIDAGEYR))
+  #  #predic the model
+  #    
+  #  fncPredictModel(data)
+  #}) 
   
   
 }
